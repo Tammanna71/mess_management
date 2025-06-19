064c3cad22f6c3fdf2d56f53857c61e3f17dd72e
Tammanna71 <farkiwalatamanna@gmail.com>
2025-06-20 00:44:08 +0530

    updated code

--- core/serializers.py
+++ core/serializers.py
@@ -1,9 +1,5 @@
 from rest_framework import serializers
 
-# class StudentSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Student 
-#         fields = '__all__'
 from .models import User, Mess
 
 class UserSerializer(serializers.ModelSerializer):
